c654875145cda8d549f3dc23b80ac6306e24ea69
GitHub Action <action@github.com>
2023-11-08 15:35:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/qurugou/wayd/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/qurugou/wayd/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.qurugou.wayd.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/qurugou/wayd/services/MyProjectService.kt
+++ src/main/kotlin/com/github/qurugou/wayd/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.qurugou.wayd.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.qurugou.wayd.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
